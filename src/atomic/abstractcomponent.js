007319fab862493c9234773bef6727762ebc4450
Jakob Heuser <jakob@felocity.com>
2013-03-18 11:28:55 -0700

    Update abstractcomponent.js

--- src/atomic/abstractcomponent.js
+++ src/atomic/abstractcomponent.js
@@ -7,16 +7,25 @@
  */
 var AbstractComponent = Fiber.extend({}, function (base) {
   return {
+    // the element bound to this object
+    ELEMENT: null,
 
+    // the events this component can emit
     events: {},
+    
+    // behaviors this component supports
     behaviors: {},
 
+    // composition dependencies. This "has" the following
+    // on load, resolved versions of these are in hasResolved
     has: [],
     hasResolved: {},
 
+    // constructor, destructor
     init: function (el /* optional */) {},
     destroy: function () {},
 
+    // event system
     on: function (name, fn) {},
     off: function (name, fn /* optional */) {},
     onAny: function (fn) {},
@@ -27,17 +36,17 @@ var AbstractComponent = Fiber.extend({}, function (base) {
     setMaxListeners: function (count) {},
     listeners: function () {},
     trigger: function (eventString, arg1, arg2...) {},
-
-    configure: function (definition, configuration) {},
-    getConfiguration: function (definition) {},
-    removeConfiguration: function (definition) {},
-
-    augment: function (definition1, definition2..., callback) {},
-    degrade: function(definition1, definition2..., callback) {},
-
     bind: function (eventing, eventName, method) {},
     unbind: function (eventing, eventName, method /* optional */) {}.
 
+    // behavior management
+    configure: function (definition, configuration) {},
+    getConfiguration: function (definition) {},
+    removeConfiguration: function (definition) {},
+    augment: function (definition1, definition2..., callback) {},
+    degrade: function(definition1, definition2..., callback) {},
+
+    // dom EL management
     attach: function (el) {},
     detatch: function () {}
     onAttach: function () {},
