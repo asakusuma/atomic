92266c85280d4c4013a5b41cdb31335c73ee56dc
Jakob Heuser <jakob@felocity.com>
2013-03-18 12:00:39 -0700

    Update carouselwithbuttons.js

--- src/modules/composites/carouselwithbuttons.js
+++ src/modules/composites/carouselwithbuttons.js
@@ -1,26 +1,30 @@
 // carouselwithbuttons
 var Atomic = require('atomic'),
     $ = require('jquery'),
-    Button = require('components/button'),
-    Carousel = require('components/carousel'),
     CarouselWithButtons;
 
 CarouselWithButtons = Atomic.Libs.Fiber.extend(Atomic.AbstractComponent, function (base) {
   return {
-    onAttach: function () {
+    has: {
+      Carousel: 'components/carousel',
+      Button: 'components/button'
+    },
+    onAttach: function (resolved) {
       var $el = $(this.ELEMENT),
           $carousel = $('.carousel', $el),
           $next = $('.next', $el),
           $prev = $('.prev', $el),
+          Carousel = resolved.Carousel,
+          Button = resolved.Button,
           carousel = new Carousel($carousel.get(0));
 
       // gotta bind them all
-      $next.each(function (next) {
-        var btn = new Button(next);
+      $next.each(function (idx, nextEl) {
+        var btn = new Button(nextEl);
         carousel.bind(btn, btn.events.USE, 'next');
       });
-      $prev.each(function (prev) {
-        var btn = new Button(prev);
+      $prev.each(function (idx, prevEl) {
+        var btn = new Button(prevEl);
         carousel.bind(btn, btn.events.USE, 'previous');
       });
     }
