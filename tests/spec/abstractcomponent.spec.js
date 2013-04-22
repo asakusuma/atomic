a2ef37528dd4c114116f67c991014a49161a177e
asakusuma <asa.kusuma@gmail.com>
2013-04-22 16:12:05 -0700

    Add test to check that AbstractComponent is defined

---
+++ tests/spec/abstractcomponent.spec.js
@@ -0,0 +1,10 @@
+/*
+ * @venus-Library mocha
+ * @venus-include ../../src/atomic/abstractcomponent.js
+ */
+
+describe('Atomic AbstractComponent', function() {
+  it('should exist', function() {
+    expect(AbstractComponent).to.be.ok();
+  });
+});
