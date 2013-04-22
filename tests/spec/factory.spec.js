7990447b6368e92ad140d49b14da9f8fecb94118
akusuma <akusuma@linkedin.com>
2013-04-22 15:30:41 -0700

    Test that Atomic Factory is defined

---
+++ tests/spec/factory.spec.js
@@ -0,0 +1,10 @@
+/*
+ * @venus-Library mocha
+ * @venus-include ../../src/atomic/factory.js
+ */
+
+describe('Atomic Factory', function() {
+  it('should be defined', function() {
+    expect(Atomic._.Factory).to.be.ok();
+  });
+});
