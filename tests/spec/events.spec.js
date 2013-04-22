415d60ca00b03d32d8a19b78bfa0025b32c37dc3
asakusuma <asa.kusuma@gmail.com>
2013-04-22 16:11:13 -0700

    Add test to check if Atomic.Events is defined

---
+++ tests/spec/events.spec.js
@@ -0,0 +1,10 @@
+/*
+ * @venus-Library mocha
+ * @venus-include ../../src/atomic/events.js
+ */
+
+describe('Atomic Events', function() {
+  it('should be defined', function() {
+    expect(Atomic.Events).to.be.ok();
+  });
+});
