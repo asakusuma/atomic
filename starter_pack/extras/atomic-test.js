57f84cd8d0de2f5bfe471598019fa84913a3e0ed
Jakob Heuser <jakob@felocity.com>
2013-10-15 18:51:01 -0700

    Passing all tests, using a universal AMD shim

--- starter_pack/extras/atomic-test.js
+++ starter_pack/extras/atomic-test.js
@@ -27,19 +27,113 @@ Atomic Components. Most notably:
   dependency injection
 */
 
+/*
+================================================================================
+ABOUT THIS FILE - amd shim https://gist.github.com/Jakobo/6894733
+================================================================================
+This file is a helper shim for pure AMD modules. When writing an AMD module,
+you need to have access to a global "define" method. This creates a global
+define(), a global require(), and a global AMD object.
+ 
+define/require work like proper sinon stubs. Any define() calls that happen
+before your test are automatically queued to run. You can then stub out
+define() to respond how you need it to, including expectations.
+ 
+AMD.run() allows you to "unpause" your loader, running through your
+define() statements as if you are doing them for the first time; this time
+with sinon stubs.
+ 
+AMD.exports is an array [] containing the exports. It's sequentially
+written to for every define() call made.
+*/
+
+var AMD = {};
+var define;
+var require;
+(function() {
+  
+  var exports = [];
+  var queue = [];
+  var oldDefine = define;
+  var oldRequire = require;
+  
+  // replace the global define if it exists with a generic buffer
+  define = function() {
+    queue.push([].slice.call(arguments, 0));
+  };
+  define.amd = true;
+  
+  var resolveDefineCall = function(args) {
+    var resolved = null;
+    var factory = null;
+    
+    // swap the "factory" object if it is a function with an interceptor
+    // this saves all the "resolved" variables transparently to the end
+    // user
+    factory = args.pop();
+    if (typeof factory == 'function') {
+      args.push(function() {
+        resolved = [].slice.call(arguments, 0);
+      });
+    }
+    
+    // through sinon we go!
+    define.apply(define, args);
+    
+    // if the factory was a function, run it with our resolved things
+    // otherwise, return the object literal
+    if (typeof factory == 'function') {
+      exports.push(factory.apply(factory, resolved));
+    }
+    else {
+      exports.push(factory);
+    }
+  };
+  
+  // initializes a sinon version of the AMD environment
+  AMD.init = function(stubber) {
+    if (!stubber && !sinon) {
+      throw new Error('AMD Shim requires at least some mock generator. We recommend sinon.js');
+    }
+    if (!stubber) {
+      stubber = sinon.stub;
+    }
+    define = stubber();
+    require = stubber();
+    AMD.ready = true;
+  };
+  
+  // tun through all our define() statements now that we've stubbed them out
+  // exports end up in AMD.exports
+  AMD.run = function() {
+    if (!AMD.ready) {
+      throw new Error('You must call AMD.init() to stub define and require first');
+    }
+
+    // empty the buffer, and shove each item through sinon.js
+    // a while loop ensures more define() goes through sinon
+    var args = queue.shift();
+    while (args) {
+      resolveDefineCall(args);
+      args = queue.shift();
+    }
+  };
+  
+  AMD.restore = function() {
+    AMD.ready = false;
+    define = oldDefine;
+    require = oldRequire;
+  };
+  
+  // all exports from define()d items
+  AMD.exports = exports;
+}(typeof sinon != 'undefined'));
+
 Atomic.version = 'TEST-' + Atomic.version;
 Atomic.Test = {};
 Atomic.Test.methods = {};
 Atomic.Test.methods.Component = Atomic.Component;
-
-/**
- * resets the environment, making all dependencies invalid again
- * useful in an after() or cleanup() type method within your unit test
- * @method Atomic.Test.resetEnv
- */
-Atomic.Test.resetEnv = function() {
-  Atomic._.modules = {};
-};
+Atomic.Test.methods.Load = Atomic.Load;
 
 /**
  * Pre-resolve an outcome for Atomic.load()
@@ -51,16 +145,6 @@ Atomic.Test.resetEnv = function() {
  */
 Atomic.Test.resolve = function(name, obj) {
   Atomic(name, [], function() { return obj; });
-};
-
-/**
- * Get a module, useful for testing. Since all tests avoid async operations
- * this can grab a module by it's defined module id
- * @method Atomic.Test.getModule
- * @param {String} id - the id of module to get
- */
-Atomic.Test.getModule = function(id) {
-  return Atomic._.modules[id];
 };
 
 /**
@@ -84,7 +168,7 @@ Atomic.Test.fakeComponent = function(def) {
     depends: [],
     name: 'Mock of ' + def.id,
     events: {},
-	init: function() {}
+    init: function() {}
   };
 
   var newFn = function() {
@@ -107,16 +191,6 @@ Atomic.Test.fakeComponent = function(def) {
 
   var component = Atomic.Component(obj);
   return component;
-};
-
-/**
- * Current a placeholder, passes through to Atomic.Component
- * It is likely we will need to alter the way component generation
- * works, but it's unknown in which ways yet
- * @method Atomic.Component
- */
-Atomic.Component = function(definition) {
-  return Atomic.Test.methods.Component(definition);
 };
 
 /**
--- tests/starter_pack/components/button.js
+++ tests/starter_pack/components/button.js
@@ -2,21 +2,31 @@
 /*
  * @venus-library qunit
  * @venus-include ../../_resources/jquery-1.9.1.min.js
+ * @venus-include ../../_resources/sinon.js
  * @venus-include ../../../dist/recent/atomic.js
  * @venus-include ../../../starter_pack/extras/atomic-test.js
  * @venus-include ../../../starter_pack/components/button.js
  */
 
-module('starter pack button');
+module('starter pack button', {
+  setup: function() {
+    AMD.init();
+    define.callsArgWith(2, Atomic.Component);
+  },
+  teardown: function() {
+    AMD.restore();
+  }
+});
 asyncTest('exists', 1, function() {
-  var Button = Atomic.Test.getModule('components/button');
-  
+  AMD.run();
+  var Button = AMD.exports[0];
   ok(Button, 'exists');
   start();
 });
 
 asyncTest('interaction', 1, function() {
-  var Button = Atomic.Test.getModule('components/button');
+  AMD.run();
+  var Button = AMD.exports[0];
   
   $('body').append('<button id="yay">yay</button>');
   var button = new Button($('#yay').get(0));
